da7ea8efab25ebae23cd89ed5fed4d4f0e29ab01
Ronan Breen <X00152190@mytudublin.ie>
2020-07-25 09:29:35 +0100

    Added FixtureStatsViewModel

---
+++ Rovers4/ViewModels/FixtureStatsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Rovers4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rovers4.ViewModels
+{
+    public class FixtureStatsViewModel
+    {
+        public IEnumerable<Fixture> Fixtures { get; set; }
+
+        public IEnumerable<Person> Staff { get; set; }
+
+        public IEnumerable<PlayerStat> Stats { get; set; }
+
+        public IEnumerable<Team> Teams { get; set; }
+
+        public string CurrentFixture { get; set; }
+
+        public string CurrentTeam { get; set; }
+
+
+
+    }
+}
