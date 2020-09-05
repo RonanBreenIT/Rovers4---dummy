2ebe797a9d43848c986ef9ce3f514a561c44c3cd
Ronan Breen <X00152190@mytudublin.ie>
2020-09-05 16:47:28 +0100

    Added Contact page and Google maps API to show club location

--- Rovers4/Controllers/HomeController.cs
+++ Rovers4/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Rovers4.Data;
 using Rovers4.Models;
 
 namespace Rovers4.Controllers
@@ -12,10 +14,12 @@ namespace Rovers4.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ClubContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ClubContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -26,6 +30,11 @@ namespace Rovers4.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Contact()
+        {
+            return View(await _context.Clubs.ToListAsync());
         }
 
         // This is how to turn off caching
